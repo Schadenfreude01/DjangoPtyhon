3586b9a36186762bb7bf586d08520f854df0ff47
famonzon <fabianalexis06@gmail.com>
2020-08-19 11:38:10 -0400

    Modelo SubCategoria

--- inv/models.py
+++ inv/models.py
@@ -13,9 +13,28 @@ class Categoria(ClaseModelo):
     def __str__(self):
         return '{}'.format(self.descripcion)
 
-    # def save(self):
-    #     self.descripcion = self.descripcion.upper() #Guardar la descripcion el mayuscula
-    #     super(Categoria.descripcion).save()
+    def save(self):
+        self.descripcion = self.descripcion.capitalize() #Coloca la descripcion con la primera letra en mayuscula
+        super(Categoria, self).save()
 
     class Meta:
         verbose_name_plural = 'Categorias'
+
+
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.CharField(
+        max_length = 100,
+        help_text = 'Descripcion de la Categoria'
+    )
+
+    def __str__(self):
+        return '{}:{}'.format(self.categoria.descripcion, self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.capitalize()
+        super(SubCategoria, self).save()
+
+    class Meta:
+        verbose_name_plural = 'Sub Categorias'
+        unique_together = ('categoria', 'descripcion') #Unique Compuesto
