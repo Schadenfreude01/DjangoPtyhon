b88e4be94d103476d7a054b1fc717f16fd13b044
famonzon <fabianalexis06@gmail.com>
2020-08-27 15:20:31 -0400

    Creacion de Producto

--- inv/models.py
+++ inv/models.py
@@ -75,3 +75,28 @@ class UM(ClaseModelo):
 
     class Meta():
         verbose_name_plural = 'Unidades de Medida'
+
+
+
+class Producto(ClaseModelo):
+    codigo = models.CharField(max_length = 100, unique = True)
+    codigo_barra = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    precio = models.FloatField(default=0)
+    existencia = models.IntegerField(default=0)
+    ultima_compra = models.DateField(null=True, blank=True)
+
+    marca = models.ForeignKey(Marca, on_delete = models.CASCADE)
+    unidad_medida = models.ForeignKey(UM, on_delete = models.CASCADE)
+    subcategoria = models.ForeignKey(SubCategoria, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+
+    def save(self):
+        self.descripcion = self.descripcion.capitalize()
+        super(Producto, self).save()
+
+    class Meta:
+        verbose_name_plural = 'Productos'
+        unique_together = ('codigo', 'codigo_barra')
