0632272b7af4056e39894f750c7f6b6b6426746e
famonzon <fabianalexis06@gmail.com>
2020-09-02 16:38:15 -0400

    Carga de jquery confirm y sin privilegios.html

--- bases/views.py
+++ bases/views.py
@@ -7,3 +7,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
     login_url = 'bases:login'
+
+class HomeSinPrivilegios(generic.TemplateView):
+    template_name = 'bases/sin_privilegios.html'
